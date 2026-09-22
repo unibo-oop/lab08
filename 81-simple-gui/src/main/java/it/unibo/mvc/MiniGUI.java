9371dbbbba54d25482e7634e8254f2255e574e02
DanySK <danilo.pianini@gmail.com>
2026-09-22 16:06:22 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@92a79ceac1a1033d08bf95a7029f6e3d65c8382a 🚀

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -78,11 +78,8 @@ public class MiniGUI {
 
     /**
      * Launches the application.
-     *
-     * @param args
-     *            ignored
      */
-    public static void main(final String... args) {
+    public static void main() {
         new MiniGUI().display();
     }
 
--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -15,7 +15,6 @@ public final class LaunchApp {
     /**
      * Runs the application.
      *
-     * @param args ignored
      * @throws ClassNotFoundException if the fetches class does not exist
      * @throws NoSuchMethodException if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
@@ -23,7 +22,7 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main() {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
         app.addView(new DrawNumberSwingView());
